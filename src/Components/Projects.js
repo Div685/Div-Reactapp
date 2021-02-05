c82a5b334bfd985427f14577656a05c5dd498b3e
Div685 <divyeshpatel685@gmail.com>
2021-02-05 11:23:40 +0300

    update projects.js file
    - Added some styling to project sections

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,19 +7,16 @@ class Projects extends Component {
     if(this.props.data) {
       var projects = this.props.data.projects.map(function(projects){
         return (
-          <div key={projects.title}>
-          <figure>
-            <img src={projects.image} alt="screenshot"/>
-            <figcaption> {projects.title} </figcaption>
-          </figure>
-          <div className="bottom-links">
-            <p> this is: {projects.title} </p>
-            <p>!PeTsLifestyLE The website is about reading and writing an article 
-              about pets. Users can also vote and comment on an 
-              article but to do that users need to signup and login.</p>
-            <span>Github Repo</span>
-            <span>Live Demo</span>
-          </div>
+          <div key={projects.title} class="project-details">
+            <figure>
+              <img src={projects.image} alt="screenshot"/>
+              <figcaption> {projects.title} </figcaption>
+            </figure>
+            <div className="bottom-links">
+              <p>{projects.description}.</p>
+              <span class="btn-git"> <a href={projects.giturl}> Github Repo </a> </span>
+              <span class="btn-live"> <a href={projects.livelink}> Live Demo </a></span>
+            </div>
           </div>
         )
       })
