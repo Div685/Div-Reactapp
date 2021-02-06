03f330eee002ab2e61d25f030ff2167b932c2f0b
Div685 <divyeshpatel685@gmail.com>
2021-02-06 21:54:29 +0300

    update header section
    - make it responsive

---
+++ src/Components/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class About extends Component {
+  render() {
+    return (
+      <div class="main-about" id ="about">
+        
+      </div>
+    )
+  }
+}
+
+export default About
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,15 +3,36 @@ import './Header.css';
 import { Link } from "react-router-dom";
 
 class Header extends Component {
+  openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginRight = "0";
+  }
+
+  closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
+  }
 
   render() {
     return (
-      <nav id="">
+      <nav id="main">
         <div>
           <span>Divyesh</span>
         </div>
-        <div>
+        <div class="rightside_nav">
         <ul>
+          <li> <a className="smoothscroll" href="#home"> Home </a></li>
+          <li> <a className="smoothscroll" href="#about">About</a></li>
+          <li> <a className="smoothscroll" href="#projects">Projects</a> </li>
+          <li> <a className="smoothscroll" href="#contact">Contact</a></li>
+        </ul>
+        <span className="menu-icon d-md-none" onClick={() => this.openNav()}>&#9776;</span>
+        </div>
+
+        {/* // <!-- nav mobile Screen--> */}
+        <div class="sidenav d-md-none d-lg-none d-flex flex-column" id="mySidenav">
+          <a href="javascript:void(0)" class="closebtn" onClick={() => this.closeNav()}>&times;</a>
+          <ul>
           <li> <a className="smoothscroll" href="#home"> Home </a></li>
           <li> <a className="smoothscroll" href="#about">About</a></li>
           <li> <a className="smoothscroll" href="#projects">Projects</a> </li>
@@ -19,6 +40,8 @@ class Header extends Component {
         </ul>
         </div>
       </nav>
+
+       
     )
   }
 }
